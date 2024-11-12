04dba8273421bf943149066fd635923f98f96367
Anna <mynameisannanas@gmail.com>
2024-11-12 14:27:43 +0100

    feat: добавляет бургер в меню

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,13 @@
+import './utils/scroll-lock'
 import { mobileVhFix } from './utils/mobile-vh-fix'
+import { addToggleMenu } from './modules/header/toggle'
 
 mobileVhFix()
+addToggleMenu()
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    // тут пока пусто
+    // тут пусто
   },
   true
 )
